13b4ec3429e508a01aca7c5a12b115f28d6c358b
c00z <zcusimano@gmail.com>
2017-01-10 18:26:25 -0800

    Updated templates

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ angular.module('campApp', ['ngRoute'])
 ////////////
 // ROUTES //
 ////////////
+
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config (  $routeProvider,   $locationProvider  )  {
+  $routeProvider //below is dot chaining
+    .when('/', {  //when on this url go to the below url and use these controllers
+      templateUrl: 'templates/books/index.html',  // this "partial" html file
+      controller: 'HomeController',         // uses this controller
+      controllerAs: 'homeCtrl'              // with this name
+    })
+
+
+
+
+  $locationProvider //deals w/ urls, telling location provider it should be using HTML5 mode.
+  .html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+}
